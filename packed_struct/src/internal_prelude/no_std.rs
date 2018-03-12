06eb597697431823f59524764def831c6159f952
Rudi Benkovic <rudi.benkovic@gmail.com>
2018-03-12 22:41:30 +0100

    no_std support, no debug formatters for now

--- packed_struct/src/internal_prelude/no_std.rs
+++ packed_struct/src/internal_prelude/no_std.rs
@@ -3,7 +3,7 @@ pub use core::marker::PhantomData;
 pub use core::iter;
 pub use core::cell::RefCell;
 pub use core::fmt;
-pub use core::fmt::Debug;
+pub use core::fmt::{Debug, Display};
 pub use core::fmt::Write as FmtWrite;
 pub use core::fmt::Error as FmtError;
 pub use core::ops::Range;
@@ -13,25 +13,4 @@ pub use core::mem;
 pub use core::intrinsics::write_bytes;
 pub use core::ops::Deref;
 pub use core::slice;
-
-
-#[cfg(feature="core_collections")]
-pub use alloc::rc::Rc;
-#[cfg(feature="core_collections")]
-pub use alloc::arc::Arc;
-#[cfg(feature="core_collections")]
-pub use alloc::boxed::Box;
-
-#[cfg(feature="core_collections")]
-pub use collections::vec::Vec;
-#[cfg(feature="core_collections")]
-pub use collections::string::*;
-#[cfg(feature="core_collections")]
-pub use collections::fmt::format as format_to_string;
-#[cfg(feature="core_collections")]
-pub use collections::fmt::{Display, Formatter};
-#[cfg(feature="core_collections")]
-pub use collections::borrow::Cow;
-#[cfg(feature="core_collections")]
-pub use collections::str::{from_utf8, FromStr};
 
