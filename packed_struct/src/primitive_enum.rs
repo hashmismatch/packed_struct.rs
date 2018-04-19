279840e8aa878557f56037d60968186d026e47a5
Rudi Benkovic <rudi.benkovic@gmail.com>
2018-04-19 10:42:11 +0200

    Support for "catch all" primitive enum packing fields.
    Implements #25

--- packed_struct/src/primitive_enum.rs
+++ packed_struct/src/primitive_enum.rs
@@ -16,3 +16,68 @@ pub trait PrimitiveEnum<T> where T: Sized + Copy + Debug, Self: Sized + Copy {
     fn all_variants() -> &'static [Self];
 }
 
+/// A wrapper for primitive enums that supports catching and retaining any values
+/// that don't have defined discriminants.
+#[derive(Copy, Clone, Debug)]
+pub enum EnumCatchAll<E, T> where E: PrimitiveEnum<T>, T: Sized + Copy + Debug {
+    /// A matched discriminant
+    Enum(E),
+    /// Some other value, stored as the primitive type
+    CatchAll(T)
+}
+
+impl<E, T> EnumCatchAll<E, T> where E: PrimitiveEnum<T>, T: Sized + Copy + Debug {
+    pub fn from_enum(v: E) -> Self {
+        EnumCatchAll::Enum(v)
+    }
+}
+
+impl<E, T> From<E> for EnumCatchAll<E, T> where E: PrimitiveEnum<T>, T: Sized + Copy + Debug {
+    fn from(v: E) -> Self {
+        EnumCatchAll::Enum(v)
+    }
+}
+
+impl<E, T> PartialEq<Self> for EnumCatchAll<E, T> where E: PrimitiveEnum<T>, T: Sized + Copy + Debug + PartialEq {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_primitive() == other.to_primitive()
+    }
+}
+
+impl<E, T> PrimitiveEnum<T> for EnumCatchAll<E, T> where E: PrimitiveEnum<T>, T: Sized + Copy + Debug {
+    fn from_primitive(val: T) -> Option<Self> {
+        match E::from_primitive(val) {
+            Some(p) => Some(EnumCatchAll::Enum(p)),
+            None => Some(EnumCatchAll::CatchAll(val))
+        }
+    }
+
+    fn to_primitive(&self) -> T {
+        match *self {
+            EnumCatchAll::Enum(p) => p.to_primitive(),
+            EnumCatchAll::CatchAll(v) => v
+        }
+    }
+
+    /// Display value, same as the name of a particular variant.
+    fn to_display_str(&self) -> &'static str {
+        // todo: needs a different API - Cow<'a, str>!
+        match *self {
+            EnumCatchAll::Enum(p) => p.to_display_str(),
+            EnumCatchAll::CatchAll(_) => "CatchAll"
+        }
+    }
+
+    fn from_str(s: &str) -> Option<Self> {
+        E::from_str(s).map(|e| EnumCatchAll::Enum(e))
+    }
+
+    fn from_str_lower(s: &str) -> Option<Self> {
+        E::from_str_lower(s).map(|e| EnumCatchAll::Enum(e))
+    }
+
+    fn all_variants() -> &'static [Self] {
+        // todo: needs a different API, not a static slice for a return value
+        &[]
+    }
+}
